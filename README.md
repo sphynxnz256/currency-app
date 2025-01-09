181fb1d007667277c073f040f780f937c0c5fcfe
sphynxnz256 <57834969+sphynxnz256@users.noreply.github.com>
2025-01-09 13:28:45 +1300

    results + font edits
    added a results section
    edited the fonts to be more responsive
    also added a task list to readme

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# currency-app
- react.js app to convert currencies using api
+# Currency Converter App
+react.js app to convert currencies using api
+
+### current things to impliment
+* need to add results ui section
+* add dummy data for currencies
+* add logic to take data and populate currency select menus
+* add logic for converting from one currency to another
+* aquire a api key to get actual currency data
+* get data from api and store it using localStorage
+* logic to only get data once per day from api otherwise use data in localStorage
+* create loading screen incase long wait for getting api data
+* input warning if user tries to enter wrong info (not sure if this is needed yet)
+* maybe create a footer with dev info
--- src/Components/Converter.jsx
+++ src/Components/Converter.jsx
@@ -1,33 +1,38 @@
 export default function Converter() {
     return(
-        <div className="converterContainer">
-            <div className="converterFromContainer">
-                <div className="converterFromContainerInner">
-                    <label for="selecterFrom" className="selectorBoxLable">Convert From:</label>
-                    <select id="selecterFrom">
-                        <option value="Currency1">Currency 1</option>
-                        <option value="Currency2">Currency 2</option>
-                        <option value="Currency3">Currency 3</option>
-                    </select>
+        <div className="outerConverterContainer">
+            <div className="converterContainer">
+                <div className="converterFromContainer">
+                    <div className="converterFromContainerInner">
+                        <label for="selecterFrom" className="selectorBoxLable">Convert From:</label>
+                        <select id="selecterFrom">
+                            <option value="Currency1">Currency 1</option>
+                            <option value="Currency2">Currency 2</option>
+                            <option value="Currency3">Currency 3</option>
+                        </select>
+                    </div>
+                </div>
+
+                <div className="inputContainer">
+                    <div className="inputContainerInner">
+                        <label>Amount:</label>
+                        <input type="number" />
+                    </div>
+                </div> 
+
+                <div className="converterToContainer">
+                    <div className="converterToContainerInner">
+                    <label for="slecterTo" className="selectorBoxLable">Convert To:</label>
+                        <select id="selecterTo">
+                            <option value="Currency1">Currency 1</option>
+                            <option value="Currency2">Currency 2</option>
+                            <option value="Currency3">Currency 3</option>
+                        </select>
+                    </div>
                 </div>
             </div>
-
-            <div className="inputContainer">
-                <div className="inputContainerInner">
-                    <label>Amount:</label>
-                    <input type="number" />
-                </div>
-            </div> 
-
-            <div className="converterToContainer">
-                <div className="converterToContainerInner">
-                <label for="slecterTo" className="selectorBoxLable">Convert To:</label>
-                    <select id="selecterTo">
-                        <option value="Currency1">Currency 1</option>
-                        <option value="Currency2">Currency 2</option>
-                        <option value="Currency3">Currency 3</option>
-                    </select>
-                </div>
+            <div className="resultsContainer">
+                <p>results go here</p>
             </div>
         </div>
     );
