0944639ebf7a0df19451ae713296acb4f8c74139
sphynxnz256 <57834969+sphynxnz256@users.noreply.github.com>
2025-01-10 17:56:59 +1300

    convert button + api data
    created a convert button with code to make the actual code. then acquired an API key to get real currency data. this data is being stored in localStorage and being used now

--- README.md
+++ README.md
@@ -5,11 +5,12 @@ react.js app to convert currencies using api
 * need to add results ui section - done
 * add dummy data for currencies - done
 * add logic to take data and populate currency select menus - done
-* add a button to initialise conversion
-* add logic for converting from one currency to another
-* aquire a api key to get actual currency data
-* get data from api and store it using localStorage
-* logic to only get data once per day from api otherwise use data in localStorage
+* add a button to initialise conversion - done
+* add logic for converting from one currency to another - done
+* aquire a api key to get actual currency data - done
+* get data from api and store it using localStorage - done
+* logic to only get data once per day from api otherwise use data in localStorage - done
+* as my api data doesnt have country names, only country codes, i need to solve for this too
 * create loading screen incase long wait for getting api data
 * input warning if user tries to enter wrong info (not sure if this is needed yet)
 * maybe create a footer with dev info
--- src/Components/Converter.jsx
+++ src/Components/Converter.jsx
@@ -1,10 +1,11 @@
 //main component to handle conversion of currencies
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 //currently dummy data
 export default function Converter() {
-    const [currencies, setCurrencies] = useState({
-        USD: { name: "US Dollar", value: 1.0 },
+    const [result, setResult] = useState("");
+    const [currencies, setCurrencies] = useState(null);
+        /*USD: { name: "US Dollar", value: 1.0 },
         EUR: { name: "Euro", value: 0.88 },
         GBP: { name: "British Pound", value: 0.76 },
         AUD: { name: "Australian Dollar", value: 1.31 },
@@ -12,8 +13,52 @@ export default function Converter() {
         JPY: { name: "Japanese Yen", value: 114.74 },
         CNY: { name: "Chinese Renminbi", value: 6.47 },
         INR: { name: "Indian Rupee", value: 74.83 },
-        BRL: { name: "Brazilian Real", value: 5.17 },
-      });
+        BRL: { name: "Brazilian Real", value: 5.17 }, */
+
+
+    //function to handle convert button click
+    function handleConvertButtonOnClick () {
+        const amountToConvert = document.getElementById("amountToConvert").value;
+        if(amountToConvert !== "") {
+            const currencyFromCode = document.getElementById("selecterFrom").value;            
+            const currencyFromValue = currencies[currencyFromCode];            
+            const currencyToCode = document.getElementById("selecterTo").value;
+            const currencyToValue = currencies[currencyToCode];            
+            const result = amountToConvert * (currencyFromValue / currencyToValue);
+            setResult(result);
+            console.log(currencyFromValue);
+        }
+    }
+
+    //useEffect to get API data and store it in localStorage
+    useEffect(() => {
+        async function fetchAPIData() {
+            const EXCHANGE_RATE_KEY = import.meta.env.VITE_EXCHANGE_RATE_API_KEY;
+            const url = `https://v6.exchangerate-api.com/v6/${EXCHANGE_RATE_KEY}/latest/USD`;
+
+            const today = (new Date()).toDateString();
+            const localKey = `EXCHANGE_RATE-${today}`;
+            if (localStorage.getItem(localKey)) {
+                const apiData = JSON.parse(localStorage.getItem(localKey));
+                setCurrencies(apiData.conversion_rates);
+                console.log(apiData.conversion_rates);
+                console.log("data fetched from cache");
+                return;
+            }
+
+            localStorage.clear();
+            try {
+                const res = await fetch(url);
+                const apiData = await res.json();
+                localStorage.setItem(localKey, JSON.stringify(apiData));
+                setCurrencies(apiData.conversion_rates);
+                console("data fetched from API");
+            } catch (error) {
+                console.log(error.message);
+            }
+        }
+        fetchAPIData();
+    }, []);
     
     //return the wepage component
     return(
@@ -23,34 +68,35 @@ export default function Converter() {
                     <label htmlFor="selecterFrom" className="selectorBoxLable">Convert From:</label>
                     <select id="selecterFrom">
                         {/*populates select menu with options*/}
-                        {Object.keys(currencies).map((key) => (
+                        {currencies && Object.keys(currencies).map((key) => (
                             <option key={key} value={key}>
-                                {currencies[key].name}
+                                {key}
                             </option>
                         ))}
                     </select>
                 </div>
 
                 <div className="inputContainer">
-                    <label htmlFor="inputCurrency" >Amount:</label>
-                    <input id="inputCurrency" type="number" />
+                    <label htmlFor="amountToConvert" >Amount:</label>
+                    <input id="amountToConvert" type="number" 
+                        placeholder="Enter Amount"/>
                 </div>
 
                 <div className="converterToContainer">
                 <label htmlFor="slecterTo" className="selectorBoxLable">Convert To:</label>
                     <select id="selecterTo">
                         {/*populates select menu with options*/}
-                        {Object.keys(currencies).map((key) => (
+                        {currencies && Object.keys(currencies).map((key) => (
                             <option key={key} value={key}>
-                                {currencies[key].name}
+                                {key}
                             </option>
                         ))}
                     </select>
                 </div>
-
             </div>
             <div className="resultsContainer">
-                <p>results go here</p>
+                <button onClick={handleConvertButtonOnClick}>Convert</button>
+                <p id="resultsText">Result: {result}</p>
             </div>
         </div>
     );
